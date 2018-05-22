323b6b5c4dfdb58c4cf67a682f91c6638336d384
Simon Wimmer <wimmers@in.tum.de>
2018-05-22 20:15:54 +0200

    Restructuring to get closer to executability & added example

--- Hidden_Markov_Model.thy
+++ Hidden_Markov_Model.thy
@@ -1,28 +1,70 @@
 theory Hidden_Markov_Model
-  imports Markov_Models.Discrete_Time_Markov_Chain Auxiliary
+  imports
+    Markov_Models.Discrete_Time_Markov_Chain Auxiliary
+    "../Dynamic_Programming_Hg/state_monad/State_Main"
+    "../Dynamic_Programming_Hg/util/Ground_Function"
+    "HOL-Library.IArray"
+    "HOL-Library.List_lexord"
+    "HOL-Library.Char_ord"
 begin
 
 section \<open>Hidden Markov Models\<close>
 
 subsection \<open>Definitions\<close>
 
-locale HMM =
+locale Closed_Kernel =
+  fixes K :: "'s \<Rightarrow> 't pmf" and S :: "'t set"
+  assumes finite: "finite S"
+      and wellformed: "S \<noteq> {}"
+      and closed: "\<forall> s. K s \<subseteq> S"
+
+locale HMM_defs =
   fixes \<K> :: "'s \<Rightarrow> 's pmf" and \<O> :: "'s \<Rightarrow> 't pmf" and \<O>\<^sub>s :: "'t set"
-  assumes observations_finite: "finite \<O>\<^sub>s"
-      and observations_wellformed: "\<O>\<^sub>s \<noteq> {}"
-      and observations_closed: "\<forall> s. \<O> s \<subseteq> \<O>\<^sub>s"
+
+locale HMM =
+  HMM_defs + O: Closed_Kernel \<O> \<O>\<^sub>s
 begin
 
-no_notation (ASCII) comp  (infixl "o" 55) 
+lemma observations_finite: "finite \<O>\<^sub>s"
+  and observations_wellformed: "\<O>\<^sub>s \<noteq> {}"
+  and observations_closed: "\<forall> s. \<O> s \<subseteq> \<O>\<^sub>s"
+  using O.finite O.wellformed O.closed by -
+
+end
+
+locale HMM2_defs = HMM_defs \<K> \<O> for \<K> :: "'s \<Rightarrow> 's pmf" and \<O> :: "'s \<Rightarrow> 't pmf" +
+  fixes \<S> :: "'s set"
+
+locale HMM2 = HMM2_defs + HMM + K: Closed_Kernel \<K> \<S>
+begin
+
+lemma states_finite: "finite \<S>"
+  and states_wellformed: "\<S> \<noteq> {}"
+  and states_closed: "\<forall> s. \<K> s \<subseteq> \<S>"
+  using K.finite K.wellformed K.closed by -
+
+end
+
+locale HMM3_defs = HMM2_defs \<O>\<^sub>s \<K> for \<O>\<^sub>s :: "'t set" and \<K> :: "'s \<Rightarrow> 's pmf" +
+  fixes state_list :: "'s list"
+
+locale HMM3 = HMM3_defs _ _ \<O>\<^sub>s \<K> + HMM2 \<O>\<^sub>s \<K> for \<K> :: "'s \<Rightarrow> 's pmf" and \<O>\<^sub>s :: "'t set" +
+  assumes state_list_\<S>: "set state_list = \<S>"
+
+context HMM_defs
+begin
+
+no_notation (ASCII) comp  (infixl "o" 55)
 
 definition
   "obs \<equiv> SOME x. x \<in> \<O>\<^sub>s"
 
-lemma obs:
+lemma (in HMM) obs:
   "obs \<in> \<O>\<^sub>s"
   unfolding obs_def using observations_wellformed by (auto intro: someI_ex)
 
-definition "K \<equiv> \<lambda> (s\<^sub>1, o\<^sub>1 :: 't). bind_pmf (\<K> s\<^sub>1) (\<lambda> s\<^sub>2. map_pmf (\<lambda> o\<^sub>2. (s\<^sub>2, o\<^sub>2)) (\<O> s\<^sub>2))"
+definition
+  "K \<equiv> \<lambda> (s\<^sub>1, o\<^sub>1 :: 't). bind_pmf (\<K> s\<^sub>1) (\<lambda> s\<^sub>2. map_pmf (\<lambda> o\<^sub>2. (s\<^sub>2, o\<^sub>2)) (\<O> s\<^sub>2))"
 
 sublocale MC_syntax K .
 
@@ -87,7 +129,7 @@ lemma T_init_observation_eq:
   apply (simp add: K_def)
   done
 
-lemma likelihood_init:
+lemma (in HMM) likelihood_init:
   "likelihood s os = T (s, obs) {\<omega> \<in> space S. L os \<omega>}"
 proof -
   have *: "(\<Sum>o\<in>\<O>\<^sub>s. emeasure (T (s, o)) {\<omega> \<in> space S. L os \<omega>}) =
@@ -163,19 +205,16 @@ lemma emeasure_T_observation_backward:
   "emeasure (T (s, o)) {\<omega> \<in> space S. L os \<omega>} = backward s os"
   using emeasure_T_observation_Cons by (induction os arbitrary: s o; simp)
 
-lemma likelihood_backward:
+lemma (in HMM) likelihood_backward:
   "likelihood s os = backward s os"
   unfolding likelihood_init emeasure_T_observation_backward ..
 
+end (* HMM Defs *)
 
-context
-  fixes \<S> :: "'s set"
-  assumes states_finite: "finite \<S>"
-      and states_wellformed: "\<S> \<noteq> {}"
-      and states_closed: "\<forall> s. \<K> s \<subseteq> \<S>"
+context HMM2
 begin
 
-fun forward where
+fun (in HMM2_defs) forward where
   "forward s t_end [] = indicator {t_end} s" |
   "forward s t_end (o # os) =
     (\<Sum>t \<in> \<S>. ennreal (pmf (\<O> t) o) * ennreal (pmf (\<K> s) t) * forward t t_end os)"
@@ -186,7 +225,7 @@ lemma forward_split:
   using that
   apply (induction os1 arbitrary: s)
   subgoal for s
-    apply (simp add: sum_indicator_mult[OF \<open>finite \<S>\<close>])
+    apply (simp add: sum_indicator_mult[OF states_finite])
     apply (subst sum.cong[where B = "{s}"])
     by auto
   subgoal for a os1 s
@@ -202,7 +241,7 @@ lemma forward_backward:
   using \<open>s \<in> \<S>\<close>
   apply (induction os arbitrary: s)
   subgoal for s
-    by (auto intro: sum_single[where k = s, OF \<open>finite \<S>\<close>])
+    by (auto intro: sum_single[where k = s, OF states_finite])
   subgoal for a os s
     apply (simp add: sum.commute sum_distrib_left[symmetric])
     apply (subst nn_integral_measure_pmf_support[where A = \<S>])
@@ -282,11 +321,11 @@ lemma max_prob_Cons':
 proof -
   let ?S = "\<lambda> os. {as. length as = length os \<and> set as \<subseteq> \<S>}"
   have S_finite: "finite (?S os)" for os :: "'t list"
-    using finite_lists_length_eq[OF \<open>finite \<S>\<close>] by (rule finite_subset[rotated]) auto
+    using finite_lists_length_eq[OF states_finite] by (rule finite_subset[rotated]) auto
   have S_nonempty: "?S os \<noteq> {}" for os :: "'t list"
   proof -
     let ?a = "SOME a. a \<in> \<S>" let ?as = "replicate (length os) ?a"
-    from \<open>\<S> \<noteq> {}\<close> have "?a \<in> \<S>"
+    from states_wellformed have "?a \<in> \<S>"
       by (auto intro: someI_ex)
     then have "?as \<in> ?S os"
       by auto
@@ -318,7 +357,7 @@ proof -
       by simp
   next
     case 2
-    from \<open>finite \<S>\<close> show ?case
+    from states_finite show ?case
       by (blast intro: S_finite)
   next
     case (3 as)
@@ -330,7 +369,8 @@ proof -
       by - (rule exI[where x = "x # as"], simp add: algebra_simps **)
   qed
   also have "\<dots> = ?r"
-    by (subst Max_image_left_mult[symmetric], fact+) (rule sym, rule Max_image_pair, fact+)
+    by (subst Max_image_left_mult[symmetric], fact+)
+       (rule sym, rule Max_image_pair, rule states_finite, fact+)
   finally show "?l = ?r" .
   have "?l' = \<integral>\<^sup>+ t'. ?f t' (t # as) (o # os) \<partial>K (s, o\<^sub>1)"
     by (rule emeasure_Collect_T[unfolded space_T]; rule measurable_V)
@@ -382,12 +422,12 @@ lemma max_prob_Nil[simp]:
 
 lemma Max_start:
   "(MAX t\<in>\<S>. (indicator {t} s :: ennreal)) = 1" if "s \<in> \<S>"
-  using \<open>finite \<S>\<close> that by (auto simp: indicator_def intro: Max_eqI)
+  using states_finite that by (auto simp: indicator_def intro: Max_eqI)
 
 lemma Max_V_viterbi:
   "(MAX t \<in> \<S>. viterbi_prob s t os) =
    Max {T (s, o) {\<omega> \<in> space S. V os as \<omega>} | as. length as = length os \<and> set as \<subseteq> \<S>}" if "s \<in> \<S>"
-  using that \<open>finite \<S>\<close> \<open>\<S> \<noteq> {}\<close>
+  using that states_finite states_wellformed
   by (induction os arbitrary: s o; simp
         add: Max_start max_prob_Cons[simplified] Max_image_commute Max_image_left_mult Max_to_image2
         cong: Max_image_cong
@@ -397,11 +437,11 @@ lemma max_prob_viterbi:
   "(MAX t \<in> \<S>. viterbi_prob s t os) = max_prob s os" if "s \<in> \<S>"
   using max_prob_init[of s os] Max_V_viterbi[OF \<open>s \<in> \<S>\<close>, symmetric] by simp
 
+end
 
 subsection \<open>Decoding the Most Probable Hidden State Sequence\<close>
 
-context
-  fixes state_list assumes "set state_list = \<S>"
+context HMM3
 begin
 
 fun viterbi where
@@ -413,7 +453,7 @@ fun viterbi where
 
 lemma state_list_nonempty:
   "state_list \<noteq> []"
-  using \<open>set state_list = \<S>\<close> \<open>\<S> \<noteq> {}\<close> by auto
+  using state_list_\<S> states_wellformed by auto
 
 lemma viterbi_viterbi_prob:
   "snd (viterbi s t_end os) = viterbi_prob s t_end os"
@@ -432,10 +472,10 @@ next
     "snd (fst (argmax snd ?xs)) = snd (argmax snd ?xs)"
     "snd (argmax snd ?xs) = (MAX x \<in> set ?xs. snd x)" .
   then show ?case
-    apply (simp add: \<open>set _ = \<S>\<close>)
+    apply (simp add: state_list_\<S>)
     apply (rule Max_eq_image_if)
-       apply (intro finite_imageI \<open>finite \<S>\<close>)
-      apply (intro finite_imageI \<open>finite \<S>\<close>)
+       apply (intro finite_imageI states_finite)
+      apply (intro finite_imageI states_finite)
     subgoal
       apply clarsimp
       subgoal for x
@@ -446,6 +486,9 @@ next
       using Cons.IH[of x] by (force split: prod.splits)
     done
 qed
+
+context
+begin
 
 private fun val_of where
   "val_of s [] [] = 1" |
@@ -495,7 +538,7 @@ next
   from argmax(1)[OF this, of snd] obtain t where "t \<in> \<S>"
     "fst (argmax snd ?xs) =
     (t # fst (viterbi t t_end os), ennreal (pmf (\<O> t) o * pmf (\<K> s) t) * snd (viterbi t t_end os))"
-    by (auto simp: \<open>set _ = \<S>\<close> split: prod.splits)
+    by (auto simp: state_list_\<S> split: prod.splits)
   with Cons.prems show ?case
     by (cases "viterbi t t_end os"; simp add: Cons.IH)
 qed
@@ -511,7 +554,7 @@ proof -
   from state_list_nonempty have "map (\<lambda> t. viterbi s t os) state_list \<noteq> []"
     by simp
   from argmax[OF this, of snd] show ?thesis
-    by (auto simp: \<open>set _ = \<S>\<close> image_comp comp_def viterbi_final_def intro: that)
+    by (auto simp: state_list_\<S> image_comp comp_def viterbi_final_def intro: that)
 qed
 
 theorem viterbi_final_max_prob:
@@ -541,10 +584,243 @@ proof -
     unfolding is_decoding_def by (simp only: T'_I_T)
 qed
 
-end (* State set as list *)
+end (* Anonymous context *)
 
-end (* Finite state set *)
+end
 
-end (* Hidden Markov Model *)
+context HMM3_defs
+begin
+
+context
+  fixes os :: "'t iarray"
+begin
+
+function viterbi_ix_inner where
+  "viterbi_ix_inner s t_end n = (if n \<ge> IArray.length os then ([], indicator {t_end} s) else
+  fst (
+    argmax snd (map
+      (\<lambda>t. let (xs, v) = viterbi_ix_inner t t_end (n + 1) in
+        (t # xs, ennreal (pmf (\<O> t) (os !! n) * pmf (\<K> s) t) * v))
+    state_list)))
+  "
+  by pat_completeness auto
+termination
+  by (relation "Wellfounded.measure (\<lambda>(_,_,n). IArray.length os - n)") auto
+
+memoize_fun viterbi_ix\<^sub>m: viterbi_ix_inner
+  with_memory dp_consistency_rbt
+  monadifies (state) viterbi_ix_inner.simps[unfolded Let_def]
+
+memoize_correct
+  by memoize_prover
+
+context
+  includes state_monad_syntax
+begin
+thm viterbi_ix\<^sub>m'.simps viterbi_ix\<^sub>m_def
+thm viterbi_ix\<^sub>m.memoized_correct
+lemmas [code] = viterbi_ix\<^sub>m'.simps
+end
+
+end (* Fixed IArray *)
+
+end (* HMM3 Defs *)
+
+context HMM3
+begin
+
+definition
+  "viterbi_ix os = viterbi_ix_inner (IArray os)"
+
+lemma viterbi_ix_drop_one:
+  "viterbi_ix (o # os) s t (n + 1) = viterbi_ix os s t n"
+  by (induction "length os - n" arbitrary: s n; simp add: viterbi_ix_def)
+
+lemma viterbi_ix_viterbi:
+  "viterbi_ix os s t 0 = viterbi s t os"
+  unfolding viterbi_ix_def
+proof (induction os arbitrary: s)
+  case Nil
+  then show ?case
+    by simp
+next
+  case (Cons o os)
+  show ?case
+    using viterbi_ix_drop_one[unfolded viterbi_ix_def]
+    by (subst viterbi.simps, subst viterbi_ix_inner.simps)
+       (simp add: Cons.IH del: viterbi_ix_inner.simps viterbi.simps)
+qed
+
+lemma viterbi_code [code]:
+  "viterbi s t os = fst (run_state (viterbi_ix\<^sub>m' (IArray os) s t 0) Mapping.empty)"
+  by (simp only: viterbi_ix_def viterbi_ix\<^sub>m.memoized_correct viterbi_ix_viterbi[symmetric])
+
+end (* Hidden Markov Model 3 *)
+
+lemma pmf_of_alist_support:
+  assumes "\<forall> (_, p) \<in> set \<mu>. p \<ge> 0"
+    and "sum_list (map snd \<mu>) = 1"
+    and "distinct (map fst \<mu>)"
+  shows "set_pmf (pmf_of_alist \<mu>) \<subseteq> fst ` set \<mu>"
+  unfolding pmf_of_alist_def
+  apply (subst set_embed_pmf)
+  subgoal for x
+    using assms(1)
+    by (auto split: option.split dest: map_of_SomeD)
+  subgoal
+    using assms
+    apply (subst nn_integral_count_space)
+    subgoal
+      by (rule finite_subset[where B = "fst ` set \<mu>"];
+          force split: option.split_asm simp: image_iff dest: map_of_SomeD)
+    apply (subst sum.mono_neutral_left[where T = "fst ` set \<mu>"])
+       apply blast
+    subgoal
+      by (smt ennreal_less_zero_iff map_of_eq_None_iff mem_Collect_eq option.case(1) subsetI)
+    subgoal
+      by auto
+
+    subgoal premises prems
+    proof -
+      have "(\<Sum>x = 0..<length \<mu>. snd (\<mu> ! x))
+      = sum (\<lambda> x. case map_of \<mu> x of None \<Rightarrow> 0 | Some v \<Rightarrow> v) (fst ` set \<mu>)"
+        apply (rule sym)
+        apply (rule sum.reindex_cong[where l = "\<lambda> i. fst (\<mu> ! i)"])
+          apply (auto split: option.split)
+        subgoal
+          by (smt atLeastLessThan_iff distinct_conv_nth inj_onI length_map nth_map prems(3))
+        subgoal
+          by (metis (no_types, lifting) atLeast0LessThan fst_conv in_set_conv_nth lessThan_iff rev_image_eqI)
+        subgoal
+          by (simp add: map_of_eq_None_iff)
+        subgoal
+          by (metis fst_conv map_of_eq_Some_iff nth_mem option.inject prems(3) prod_eqI snd_conv)
+        done
+      with prems(2) show ?thesis
+        by (smt \<open>\<And>x. 0 \<le> (case map_of \<mu> x of None \<Rightarrow> 0 | Some p \<Rightarrow> p)\<close> atLeastLessThan_iff ennreal_1 length_map nth_map sum.cong sum_ennreal sum_list_sum_nth)
+    qed
+    done
+  apply (force split: option.split_asm simp: image_iff dest: map_of_SomeD)+
+  done
+
+locale Closed_Kernel_From =
+  fixes K :: "('s \<times> ('t \<times> real) list) list"
+    and S :: "'t list"
+  assumes wellformed: "S \<noteq> []"
+      and closed: "\<forall> (s, \<mu>) \<in> set K. \<forall> (t, _) \<in> set \<mu>. t \<in> set S"
+      and is_pmf:
+        "\<forall> (_, \<mu>) \<in> set K. \<forall> (_, p) \<in> set \<mu>. p \<ge> 0"
+        "\<forall> (_, \<mu>) \<in> set K. distinct (map fst \<mu>)"
+        "\<forall> (s, \<mu>) \<in> set K. sum_list (map snd \<mu>) = 1"
+begin
+
+definition
+  "K' s \<equiv> case map_of (map (\<lambda> (s, \<mu>). (s, PMF_Impl.pmf_of_alist \<mu>)) K) s of
+  None \<Rightarrow> return_pmf (hd S) |
+  Some s \<Rightarrow> s"
+
+sublocale Closed_Kernel K' "set S"
+  using wellformed closed is_pmf pmf_of_alist_support
+  unfolding K'_def by - (standard; fastforce split: option.split_asm dest: map_of_SomeD)
+
+end
+
+locale Concrete_HMM_defs =
+  fixes \<K> :: "('s \<times> ('s \<times> real) list) list"
+    and \<O> :: "('s \<times> ('t \<times> real) list) list"
+    and \<O>\<^sub>s :: "'t list"
+    and \<K>\<^sub>s :: "'s list"
+
+definition (in Concrete_HMM_defs)
+  "\<K>' s \<equiv> case map_of (map (\<lambda> (s, \<mu>). (s, PMF_Impl.pmf_of_alist \<mu>)) \<K>) s of
+    None \<Rightarrow> return_pmf (hd \<K>\<^sub>s) |
+    Some s \<Rightarrow> s"
+
+definition (in Concrete_HMM_defs)
+  "\<O>' s \<equiv> case map_of (map (\<lambda> (s, \<mu>). (s, PMF_Impl.pmf_of_alist \<mu>)) \<O>) s of
+    None \<Rightarrow> return_pmf (hd \<O>\<^sub>s) |
+    Some s \<Rightarrow> s"
+
+locale Concrete_HMM = Concrete_HMM_defs +
+  assumes observations_wellformed': "\<O>\<^sub>s \<noteq> []"
+      and observations_closed': "\<forall> (s, \<mu>) \<in> set \<O>. \<forall> (t, _) \<in> set \<mu>. t \<in> set \<O>\<^sub>s"
+      and observations_form_pmf':
+        "\<forall> (_, \<mu>) \<in> set \<O>. \<forall> (_, p) \<in> set \<mu>. p \<ge> 0"
+        "\<forall> (_, \<mu>) \<in> set \<O>. distinct (map fst \<mu>)"
+        "\<forall> (s, \<mu>) \<in> set \<O>. sum_list (map snd \<mu>) = 1"
+  assumes states_wellformed: "\<K>\<^sub>s \<noteq> []"
+      and states_closed: "\<forall> (s, \<mu>) \<in> set \<K>. \<forall> (t, _) \<in> set \<mu>. t \<in> set \<K>\<^sub>s"
+      and states_form_pmf:
+        "\<forall> (_, \<mu>) \<in> set \<K>. \<forall> (_, p) \<in> set \<mu>. p \<ge> 0"
+        "\<forall> (_, \<mu>) \<in> set \<K>. distinct (map fst \<mu>)"
+        "\<forall> (s, \<mu>) \<in> set \<K>. sum_list (map snd \<mu>) = 1"
+begin
+
+interpretation O: Closed_Kernel_From \<O> \<O>\<^sub>s
+  rewrites "O.K' = \<O>'"
+proof -
+  show \<open>Closed_Kernel_From \<O> \<O>\<^sub>s\<close>
+    using observations_wellformed' observations_closed' observations_form_pmf'
+    by unfold_locales auto
+  show \<open>Closed_Kernel_From.K' \<O> \<O>\<^sub>s = \<O>'\<close>
+    unfolding Closed_Kernel_From.K'_def[OF \<open>Closed_Kernel_From \<O> \<O>\<^sub>s\<close>] \<O>'_def
+    by auto
+qed
+
+interpretation K: Closed_Kernel_From \<K> \<K>\<^sub>s
+  rewrites "K.K' = \<K>'"
+proof -
+  show \<open>Closed_Kernel_From \<K> \<K>\<^sub>s\<close>
+    using states_wellformed states_closed states_form_pmf by unfold_locales auto
+  show \<open>Closed_Kernel_From.K' \<K> \<K>\<^sub>s = \<K>'\<close>
+    unfolding Closed_Kernel_From.K'_def[OF \<open>Closed_Kernel_From \<K> \<K>\<^sub>s\<close>] \<K>'_def
+    by auto
+qed
+
+sublocale HMM_interp: HMM3 \<O>' "set \<K>\<^sub>s" \<K>\<^sub>s \<K>' "set \<O>\<^sub>s"
+  using O.Closed_Kernel_axioms K.Closed_Kernel_axioms
+  by (intro_locales, intro HMM3_axioms.intro HOL.refl)
+
+end (* Concrete HMM *)
+
+subsection \<open>The Ice Cream Example\<close>
+
+definition
+  "states = [''start'', ''hot'', ''cold'', ''end'']"
+
+definition observations :: "int list" where
+  "observations = [1, 2, 3]"
+
+definition
+  "kernel =
+    [
+      (''start'', [(''hot'',0.8 :: real), (''cold'',0.2)]),
+      (''hot'',   [(''hot'',0.6 :: real), (''cold'',0.3), (''end'', 0.1)]),
+      (''cold'',  [(''hot'',0.4 :: real), (''cold'',0.5), (''end'', 0.1)]),
+      (''end'',   [(''end'', 1)])
+    ]"
+
+definition
+  "emissions =
+    [
+      (''start'', [(1, 1::real)]),
+      (''hot'',   [(1, 0.2), (2, 0.4), (3, 0.4)]),
+      (''cold'',  [(1, 0.5), (2, 0.4), (3, 0.1)]),
+      (''end'',   [(1, 1::real)])
+    ]
+  "
+
+global_interpretation Concrete_HMM kernel emissions observations states
+  defines viterbi = HMM_interp.viterbi
+  by (standard; eval)
+
+lemmas [code] = Concrete_HMM_defs.\<O>'_def Concrete_HMM_defs.\<K>'_def
+
+lemmas [code] = HMM3_defs.viterbi_ix\<^sub>m'.simps
+
+instance prod :: (linorder, linorder) linorder
+  sorry
+
+value "viterbi ''start'' ''end'' [1, 2]"
 
 end (* Theory *)
